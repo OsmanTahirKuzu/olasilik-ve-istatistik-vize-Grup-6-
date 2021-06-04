80419d208038280365136fa884cdf30eba7de8ea
Osman Tahir Kuzu <56130302+OsmanTahirKuzu@users.noreply.github.com>
2021-06-04 15:58:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,203 +3,139 @@
     var aritmetikOrt= document.querySelector('.aritmetikOrt');
     var geometrikOrt= document.querySelector('.geometrikOrt');
     var harmonikOrt= document.querySelector('.harmonikOrt');
-    var mod= document.querySelector('.mod');
-    var medyan= document.querySelector('.medyan');
-    var standartSapma= document.querySelector('.standartSapma');
-    var permutasyon= document.querySelector('.permutasyon');
-    var kombinasyon= document.querySelector('.kombinasyon');
+    var mods= document.querySelector('.mod');
+    var medyans= document.querySelector('.medyan');
+    var standartSapmas= document.querySelector('.standartSapma');
+    
 
     var dizi= document.querySelector('.sayilar');
-   
-    aritmetikOrt.addEventListener('click',function(){
-    
-       function aritmetikOrtalama(item) {
-           var ayrac= item.split(" ");
-            var total=0;
-            var cevir;
-            ayrac.forEach(item => {
-                cevir = Number(item)
-                  total+=cevir;
-            });
-    
-            return total/ayrac.length;
-            
-        } 
-        alert(aritmetikOrtalama(dizi.value));
+
+    function aritmetikOrtalama(item) {
+        var ayrac= item.split(" ");
+         var total=0;
+         var cevir;
+         ayrac.forEach(item => {
+             cevir = Number(item)
+               total+=cevir;
+         });
+ 
+         return total/ayrac.length;
          
-    })
-    geometrikOrt.addEventListener('click',function(){
-    
-        function geometrikOrtalama(item) {
-            var ayrac= item.split(" ");
-             var start=1;
-             var cevir;
-             ayrac.forEach(item => {
-                cevir = Number(item)
-                start*=cevir;
-             });
-             return Math.pow(start, (1/ayrac.length));   
-         } 
-         alert(geometrikOrtalama(dizi.value));
-          
-     })
-
-     harmonikOrt.addEventListener('click',function(){
-    
-        function harmonikOrtalama(item) {
-            var ayrac= item.split(" ");
-            var sonuc= 0;
-            var cevir;
-            
-             ayrac.forEach(item => {
-                cevir = Number(item)
-                sonuc+=(1/cevir);
-             });
-             
-             return ayrac.length / sonuc;   
-         } 
-         alert(harmonikOrtalama(dizi.value));
-          
-     })
-
-     mod.addEventListener('click',function(){
-    
-        function mod(item) {
-            
-            var i, j,z, tmp, maxCount, modeValue;
-            var ayrac= item.split(" ");
-            var tally = [ayrac.length];
-                
-                 for(i=0;i<ayrac.length;i++)
-                     {
-                           for(j=0;j<ayrac.length-i;j++)
-                           {
-                                 if(invalue[j]>invalue[j+1])
-                                 {
-                                       tmp=invalue[j];
-                                       invalue[j]=invalue[j+1];
-                                       invalue[j+1]=tmp;
-                                 }
-                           }
-                     }
-                for (i = 0; i < ayrac.length; i++) 
-                {
-                    for(z=i+1;z<ayrac.length;z++)
-                    {
-                        
-                        if(invalue[i]==invalue[z])
-                        {
-                            tally[i]++;
-                        }
-                    }
-                }
-                
-                
-                maxCount = 0;
-                modeValue = 0;
-                for (i = 1; i <= ayrac.length; i++) 
-                {
-                    if (tally[i] > maxCount) 
-                    {
-                        maxCount = tally[i];
-                        modeValue = invalue[i];
-                    }
-                }
-                return modeValue;
-                
-            
-
-
-
-
-            // var ayrac= item.split(" ");
-            // var modes = [], count = [] , i, number, maxIndex = 0;
-            
-            // for (i = 0; i < ayrac.length; i++) {
-            //     number = ayrac[i];
-            //     count[number]++;
-            //     if (count[number] > maxIndex) {
-            //         maxIndex = count[number];
-            //     }
-            // }
          
-            
-            //     for(i = 0;i<ayrac.length;i++){
-            //         if (count[i] == maxIndex) {
-            //             modes.push(i);
-            //         }
-            //     }
+    }
 
-             
-            
-            // return modes;   
-         } 
-         alert(mod(dizi.value));
-          
-     })
-     
-     medyan.addEventListener('click',function(){
-    
-        function medyan(item) {
-            var ayrac= item.split(" ");
-            var medyandeger = 0, numsLen = ayrac.length;
-            
+    function geometrikOrtalama(item) {
+        var ayrac= item.split(" ");
+         var start=1;
+         var cevir;
+         var flag = 0;
+         ayrac.forEach(item => {
+            cevir = Number(item)
 
-            ayrac.forEach(item => {
-                cevir = Number(item);
-             });
-            ayrac.sort();
-            
-            if (numsLen % 2 == 0 ) {
-                medyandeger = (ayrac[numsLen / 2 - 1] + ayrac[numsLen / 2]) / 2;
-            } else { 
-                medyandeger = ayrac[(numsLen - 1) / 2];
-            }
-          
-            return medyandeger;   
-         } 
-         alert(medyan(dizi.value));
-          
-     })
-    
-     standartSapma.addEventListener('click',function(){
-    
-        function standartSapma(item) {
-            var ayrac= item.split(" ");
-            var ortalama = aritmetikOrt(dizi.value);
-            
-             ayrac.forEach(item => {
-                 cevir = Number(item)
-                   total+=Math.pow((ortalama-cevir),2);
-             });
-     
-             return Math.sqrt(total*(1/ayrac.length-1));
-             
-         } 
-         alert(standartSapma(dizi.value));
-          
-     })
-     permutasyon.addEventListener('click',function(){
-    
-        function permutasyon() {
-            var n = Number(window.prompt("Kaç değer gireceksiniz n:",0));
-            var r = Number(window.prompt("Kaç değer gireceksiniz r :",0));
-            if(n >= r){
-                alert("işlem başarılı! Sonucu görüntülemek için tamam butonuna basınız.");
-                return factoriyel(n)/factoriyel(n-r);
-            }
-            else
+            if(cevir < 0)
             {
-                alert("n değeriniz r den küçük olamaz! Lütfen tekrar deneyiniz");
-                return permutasyon();
+                flag = 1;
+                
             }
-         } 
-             
-         
-         alert(permutasyon());
-          
-     })
+            start*=cevir;
+        });
 
+        if(flag == 0)
+        {
+            return Math.pow(start, (1/ayrac.length));
+        }
+        else 
+        {
+            alert("Girilen değerler pozitif sayılardan oluşmalıdır!");
+            return;
+            
+        }
+            
+    };   
+    
+
+    function harmonikOrtalama(item) {
+        var ayrac= item.split(" ");
+        var sonuc= 0;
+        var cevir;
+        
+         ayrac.forEach(item => {
+            cevir = Number(item)
+            sonuc+=(1/cevir);
+         });
+         
+         return ayrac.length / sonuc;   
+    }
+
+    function mod(item) {
+            
+        var ayrac=item.split(" ");
+        var sayidizi=[];
+        var modes=[];
+        var count=[];
+        var i, j;
+        var number;
+        var maxIndex=0;
+
+        for(i=0;i<ayrac.length;i++){
+            sayidizi[i]=parseInt(ayrac[i]);
+        }
+        for (j=0;j<ayrac.length;j++){
+            number=sayidizi[j];
+            count[number]=(count[number]||0)+1;
+            if(count[number]>maxIndex){
+                maxIndex=count[number];
+            }
+        }
+        for(j in count){
+            if(count.hasOwnProperty(j)){
+                if(count[j]==maxIndex){
+                    modes.push(Number(j));
+                }
+            }
+        }   
+        return modes;   
+    }
+
+    function medyan(item) {
+        var ayrac= item.split(" ");
+        var i = 0;
+        var arr = [];
+        var numsLen = ayrac.length;
+        
+        ayrac.forEach(item => {
+            cevir = parseInt(item)
+            ayrac[i] = cevir;
+            i++;  
+            
+        });
+        var siraliDizi = ayrac.sort(function(a, b){return a - b});
+        
+        alert(ayrac);
+        
+         if (numsLen % 2 == 0 ) {
+            medyandeger = (siraliDizi[numsLen / 2 - 1] + siraliDizi[numsLen / 2]) / 2;
+         } else { 
+            medyandeger = siraliDizi[(numsLen - 1) / 2];
+         }
+      
+        return medyandeger;  
+     }
+     function standartSapma(item) {
+        var ayrac= item.split(" ");
+        var ortalama = aritmetikOrtalama(item);
+        
+        var i;
+        var toplam = 0;
+        var sayidizi = [];
+        
+        for(i=0;i<ayrac.length;i++){
+            sayidizi[i]=parseInt(ayrac[i]);
+            toplam += (sayidizi[i] - ortalama)*(sayidizi[i] - ortalama);
+        }
+        return Math.sqrt(toplam/(ayrac.length-1));
+         
+     }
      function factoriyel(n)
      {
         var i;
@@ -217,23 +153,86 @@
         }
         
      }
-     kombinasyon.addEventListener('click',function(){
+     //////////////////////////////
+    aritmetikOrt.addEventListener('click',function(){
     
-        function kombinasyon() {
-            var n = Number(window.prompt("Kaç değer gireceksiniz n:",0));
-            var r = Number(window.prompt("Kaç değer gireceksiniz r :",0));
-            if(n >= r && n>=0 && r>=0){
-                alert("işlem başarılı! Sonucu görüntülemek için tamam butonuna basınız.");
-                return factoriyel(n)/(factoriyel(n-r)*factoriyel(r));
-            }
-            else
-            {
-                alert("n değeriniz r den küçük olamaz! Lütfen tekrar deneyiniz");
-                return kombinasyon();
-            }
-         } 
+        alert(aritmetikOrtalama(dizi.value));
          
-         alert(kombinasyon());
+    })
+    geometrikOrt.addEventListener('click',function(){
+    
+        
+         alert(geometrikOrtalama(dizi.value));
           
      })
+
+     harmonikOrt.addEventListener('click',function(){
+    
+         
+         alert(harmonikOrtalama(dizi.value));
+          
+     })
+
+     mods.addEventListener('click',function(){
+    
+         
+         alert(mod(dizi.value));
+          
+     })
+     
+     medyans.addEventListener('click',function(){
+    
+         
+         alert(medyan(dizi.value));
+          
+     })
+    
+     standartSapmas.addEventListener('click',function(){
+    
+         alert(standartSapma(dizi.value));
+          
+     })
+
+     
+
+     
+
+     function permutasyon() {
+        var n = Number(window.prompt("Eleman Sayısı (n):",0));
+        var r = Number(window.prompt("Seçim Sayısı (r) :",0));
+        if(n<0 || r<0){
+            alert("n ve r değerleri negatif olamaz.");
+            return permutasyon(); 
+        }
+        if(n >= r){
+            alert("işlem başarılı! Sonucu görüntülemek için tamam butonuna basınız.");
+            alert(factoriyel(n)/factoriyel(n-r));
+        }
+        else
+        {
+            alert("n değeriniz r den küçük olamaz! Lütfen tekrar deneyiniz");
+            return permutasyon();
+        }
+     } 
+     
+     
+     function kombinasyon() {
+        var n = Number(window.prompt("Eleman Sayısı (n):",0));
+        var r = Number(window.prompt("Seçim Sayısı (r) :",0));
+        if(n<0 || r<0){
+            alert("n ve r değerleri negatif olamaz.");
+            return kombinasyon(); 
+        }
+        if(n >= r && n>=0 && r>=0){
+            alert("işlem başarılı! Sonucu görüntülemek için tamam butonuna basınız.");
+            alert(factoriyel(n)/(factoriyel(n-r)*factoriyel(r)));
+            
+        }
+        else
+        {
+            alert("n değeriniz r den küçük olamaz! Lütfen tekrar deneyiniz");
+            return kombinasyon();
+        }
+     } 
+    
 
